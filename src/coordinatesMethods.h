1f33220b3547d07adb06599b69ce458dfe346d82
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-06-07 19:13:00 +0200

    Create coordinatesMethods.h

---
+++ src/coordinatesMethods.h
@@ -0,0 +1,47 @@
+#include <time.h>
+#include <math.h>
+#include <stdint.h>
+#define M_PI  3.14159265358979323846
+#define TOWER_LATITUDE 55.95045 
+#define TOWER_LONGITUDE -3.36061
+
+#pragma pack(push, 1)
+typedef struct{
+	float latitude;
+	float longitude;
+	uint16_t altitude;
+	char message[54];
+} Coordinates;
+#pragma pack(pop)
+
+float meters_to_lat(float meters){
+    return meters/111000.0;
+};
+
+float meters_to_long(float meters, float lat){
+    float lat_rad = lat * (M_PI /180.0);
+    return meters / (111000.0 * cos(lat_rad));
+}
+
+int generateRadius(){
+    return rand()%10000+1;
+}
+
+uint16_t generateAltitude(){
+    return (uint16_t) rand()%10000+500;
+}
+
+Coordinates generatePoint(){
+    float randAngle = ((float)rand()/RAND_MAX)*2.0 * M_PI;
+    int randRadius = generateRadius();
+    uint16_t randAlt = generateAltitude();
+
+    float offsLAT = meters_to_lat(randRadius * sin(randAngle));
+    float offsLONG = meters_to_long(randRadius * cos(randAngle), TOWER_LATITUDE);
+
+    float randLat = TOWER_LATITUDE + offsLAT;
+    float randLong = TOWER_LONGITUDE + offsLONG;
+
+    Coordinates mycoor = {.latitude=randLat, .longitude=randLong,.altitude=randAlt,.message="AFIS TX COOR TO TWR"};
+    return mycoor;
+} 
