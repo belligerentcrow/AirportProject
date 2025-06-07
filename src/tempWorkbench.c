307f0255ca0568e1bcf2c4ef83682ddae1a40f73
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-06-07 19:13:15 +0200

    Update tempWorkbench.c

--- src/tempWorkbench.c
+++ src/tempWorkbench.c
@@ -5,17 +5,13 @@
 #include <time.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
+#include "coordinatesMethods.h"
 #include "lib-misc.h"
 
-typedef struct  {
-    char planeCode[7];
-    uint32_t timestamp;
-    float latitude;
-    float longitude;
-    uint8_t altitude;
-    char departure[5];
-    char arrival[5];
-} PlaneData;
+#define M_PI  3.14159265358979323846
+#define TOWER_LATITUDE 55.95045 
+#define TOWER_LONGITUDE -3.36061
 
 typedef struct{
 	char flightcode[7];
@@ -25,7 +21,16 @@ typedef struct{
 	uint16_t altitude;
 	char departure[5];
 	char arrival[5];
-} airplaneInfo;
+    char message[32];
+} AirplaneInfo;
+
+typedef struct {
+	char flightcode[7];
+	uint32_t timestamp;
+	uint8_t alertLevel;
+    char message[48];
+} AlertData;
+
 
 //infos eee = {43.03129, "LIFO", "FIFO", 128};
 int main(int argc, char * argv[]){
@@ -33,12 +38,13 @@ int main(int argc, char * argv[]){
         printf("Usage: %s <PLANECODE (6 CHAR)>\n",argv[0]);
         exit(EXIT_FAILURE);
     }
-
-    printf("Sizeof argv[1]: %ld \n", strlen(argv[1]));
+    srand(time(NULL));
+    printf("argv[1] = %s, Sizeof argv[1]: %ld \n", argv[1], strlen(argv[1]));
     time(0);
-    airplaneInfo flight={"BRO420",time(0),12.3842,29.9842,5000,"LPAD","FHHE"};
-    //printf("%4.4f, %s, %s\n", eee.flolo, eee.departure2, eee.departure2);
-    //printf("%ld, %ld, %ld, %lu and total struct weight: %ld\n", sizeof(eee.flolo), sizeof(eee.departure2), sizeof(eee.arrival2), sizeof(eee.jeje), sizeof(eee));
+    Coordinates mycoordinates = generatePoint();
+    
+    AirplaneInfo flight = {.timestamp=time(NULL), .altitude=mycoordinates.altitude, .latitude=mycoordinates.latitude, .longitude=mycoordinates.longitude, .departure="LAXP", .arrival="EDNB"};
+    strcpy(flight.flightcode, argv[1]);
     printf("flightcode: %s, size: %ld\n", flight.flightcode, sizeof(flight.flightcode));
     printf("timestamp: %u, size: %ld\n", flight.timestamp, sizeof(flight.timestamp));
     printf("latitude: %4.4f, size: %ld\n", flight.latitude, sizeof(flight.latitude));
@@ -47,7 +53,13 @@ int main(int argc, char * argv[]){
     printf("departure: %s, size: %ld\n", flight.departure, sizeof(flight.departure));
     printf("arrival: %s, size: %ld\n", flight.arrival, sizeof(flight.arrival));
     printf("flight entire size: %ld\n", sizeof(flight));
+    printf("Size of 2 floats + 1 int: %ld\n\n", sizeof(float)+sizeof(float)+sizeof(uint16_t));
 
-
+    printf("Size of AlertData: %ld \n", sizeof(AlertData));
+    printf("Size of AirplaneInfo: %ld \n", sizeof(AirplaneInfo));
+    printf("Size of Coordinates: %ld \n", sizeof(Coordinates));
+    printf("Generating coordinates...\n\n");
+    Coordinates newcoor = generatePoint();
+    printf("coordinates: %4.6f, %4.6f, %u, %s --- Sizeof coordiantes = %ld\n", newcoor.latitude, newcoor.longitude, newcoor.altitude, newcoor.message, sizeof(newcoor));
 
 }
