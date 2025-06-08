00ce9bca6eefeb63dae1e04e8d14ec609460307b
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-06-08 12:47:16 +0200

    Update coordinatesMethods.h

--- src/coordinatesMethods.h
+++ src/coordinatesMethods.h
@@ -19,11 +19,27 @@ typedef struct{
 
 float meters_to_lat(float meters){
     return meters/111000.0;
-};
+}
 
 float meters_to_long(float meters, float lat){
     float lat_rad = lat * (M_PI /180.0);
     return meters / (111000.0 * cos(lat_rad));
+}
+
+float deg2rad(float degree){
+    return (degree * M_PI /180.0);
+}
+
+//Haversine 
+double calcdistance(double lat1, double lon1, double lat2, double lon2){
+    double lat1r, lon1r, lat2r, lon2r, u, v;
+    lat1r = deg2rad(lat1);
+    lon1r = deg2rad(lon1);
+    lat2r = deg2rad(lat2);
+    lon2r = deg2rad(lon2);
+    u = sin((lat2r - lat1r)/2);
+    v = sin((lon2r - lon1r)/2);
+    return 2.0 * 6371.0 * asin(sqrt(u*u+cos(lat1r)*cos(lat2r) *v *v));
 }
 
 int generateRadius(){
